6f38c9c3830a1c50084869a64d527a7391605362
Evan Allen <abzman2000@gmail.com>
2021-08-11 23:48:43 -0400

    swap serial pins for esp8266
    I based my hardware for the esp8266 on the jee-link, these configurations work with that

--- Serial.ino
+++ Serial.ino
@@ -17,6 +17,15 @@ void serial_setup()
     Serial.end();
   }
   Serial.begin(current_config.baudrate);
+#if defined(ESP8266)
+#pragma message "ESP8266 stuff happening!"
+  Serial.swap();
+#elif defined(ESP32)
+#pragma message "ESP32 stuff happening!"
+#else
+#error "This ain't a ESP8266 or ESP32, dumbo!"
+#endif
+
   serial_started = true;
 
   pinMode(LED_PIN, OUTPUT);
